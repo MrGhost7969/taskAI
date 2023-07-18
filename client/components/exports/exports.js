0e2b5fec7b8c4886e95399daf524573aa0d00c3d
Franco <bigghostyman1234@gmail.com>
2023-07-18 18:02:57 -0400

    Update gitignore

---
+++ client/components/exports/exports.js
@@ -0,0 +1,13 @@
+export const route = {
+    dev: ""
+}
+export let cardArr = [
+    { uri: 'https://picsum.photos/700', title: "titled untitled" },
+    { uri: 'https://picsum.photos/700', title: "Hello World" }
+]
+export let privPageArr = [
+    { uri: 'https://picsum.photos/700', title: "page of something" },
+    { uri: 'https://picsum.photos/700', title: "Software Development" }
+]
+
+export let arrTDs = [{ label: "Lorem ipsum tomi" }, { label: "ya boi stank oml" }, { label: "spaghetti ohs zos" }];
